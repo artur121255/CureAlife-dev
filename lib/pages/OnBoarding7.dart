0bad6e9be53b3f8e72e0f697d5c361a41b69bd4e
artur yaski <arturyaski96@gmail.com>
2022-10-19 22:14:47 +0300

    BotomNav

--- lib/pages/OnBoarding7.dart
+++ lib/pages/OnBoarding7.dart
@@ -265,6 +265,25 @@ class OnBoarding7State extends State<OnBoarding7>
           ]),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+        onTap: null,
+      ),
     );
   }
-}
+  }
+
